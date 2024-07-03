e0239bce15e7c8c3173bd67dc8b2acb7dc593a3d
Drackion <64641342+Drackion@users.noreply.github.com>
2024-07-03 13:41:07 +1000

    regex fixes, from kjm

--- kubejs/server_scripts/mods/Modern Industrialization/Aluminum.js
+++ kubejs/server_scripts/mods/Modern Industrialization/Aluminum.js
@@ -2,7 +2,9 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.recipes(event => {
-  event.remove({ id: /alltheores:.*aluminum.*/})
+  event.remove({ id: /alltheores:.*aluminum_ingot_from.*/, not: {id: 'alltheores:aluminum_ingot_from_block'}})
+  event.remove({ id: /alltheores:.*aluminum_dust_from.*/})
+  event.remove({ id: /alltheores:mek_processing\/aluminum.*/})
   event.remove({id: /xycraft_world:.*aluminum.*/})
   event.remove({id: /.*bauxite.*/})
 })
