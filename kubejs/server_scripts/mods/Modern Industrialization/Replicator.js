2d60cc86bc2ba740d69e5576a1ad9be9ecd05c67
djnifos <djnifos@gmail.com>
2024-07-03 10:55:00 -0500

    Prevent Cardboard box replication

---
+++ kubejs/server_scripts/mods/Modern Industrialization/Replicator.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('item', event => {
+    // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+    event.add('modern_industrialization:replicator_blacklist', 'mekanism:cardboard_box')
+})  
