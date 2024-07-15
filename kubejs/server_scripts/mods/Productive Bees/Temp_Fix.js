dfef86a7cd73cadd10e672bf50ae985a6d35ebcd
Kevin-Marsh <Kevin-Marsh@users.noreply.github.com>
2024-07-16 04:26:57 +1000

    Temporary fix for MI bees

---
+++ kubejs/server_scripts/mods/Productive Bees/Temp_Fix.js
@@ -0,0 +1,10 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.tags('block', event => {
+    event.add('c:storage_blocks/titanium', 'modern_industrialization:titanium_block')
+    event.add('c:storage_blocks/tungsten', 'modern_industrialization:tungsten_block')
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
