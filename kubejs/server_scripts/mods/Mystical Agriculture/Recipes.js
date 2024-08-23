9ebd208d71cdf457f4b33d90613a755451f96ec5
micrwvae <mmm.kloevers@gmx.de>
2024-08-23 09:23:23 +0200

    exchanged tall for short grass
    edited the earth agglomeratio recipe to use short grass

---
+++ kubejs/server_scripts/mods/Mystical Agriculture/Recipes.js
@@ -0,0 +1,17 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+
+    allthemods.remove({ id: 'mysticalagriculture:earth_agglomeratio'})
+    allthemods.shaped('mysticalagriculture:earth_agglomeratio', ['GF', 'DC'], {
+        G: 'minecraft:short_grass',
+        F: 'minecraft:gravel',
+        D: 'minecraft:dirt',
+        C: 'minecraft:clay_ball'
+    })
+    
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
