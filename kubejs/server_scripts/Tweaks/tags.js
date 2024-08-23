2e29c5a4379afc9ab7e7f12f3193430daaede379
Toblerone0508 <tobybrent10@gmail.com>
2024-08-23 21:27:33 +1000

    Adds some Missing Tags
    Just a few tags that are missing from some items, I've asked the mod's devs about adding most of them but for now this works. I will definitely add more as I see more.

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -4,5 +4,39 @@
 ServerEvents.tags('block', allthemods => {
   allthemods.add('ftbchunks:interact_whitelist', ['@waystones'])
 })
+
+ServerEvents.tags('item', allthemods => {
+allthemods.add('minecraft:beacon_base_blocks', [
+  'allthemodium:vibranium_allthemodium_alloy_block',
+  'allthemodium:unobtainium_allthemodium_alloy_block',
+  'allthemodium:unobtainium_vibranium_alloy_block',
+  'allthetweaks:atm_star_block'
+])
+allthemods.add('c:dusts', [
+  'alltheores:netherite_dust',
+  'extended_industrialization:granite_dust',
+  'productivebees:draconic_dust',
+  'railcraft:saltpeper_dust',
+  'railcraft:ender_dust'
+])
+  'megacells:sky_steel_ingot',
+  'megacells:sky_bronze_ingot',
+  'megacells:sky_osmium_ingot',
+  'bigreactors:ludicrite_ingot',
+  'bigreactors:ridiculite_ingot',
+  'bigreactors:inanite_ingot',
+  'bigreactors:insanite_ingot'
+allthemods.add('minecraft:head_armor', [
+    'allthemodium:allthemodium_helmet',
+    'allthemodium:vibranium_helmet',
+    'allthemodium:unobtainium_helmet'
+  ])
+  allthemods.add('c:dusts/netherite', 'alltheores:netherite_dust')
+allthemods.add('c:dusts/granite', 'extended_industrialization:granite_dust')
+allthemods.add('c:dusts/ender_pearl', 'railcraft:ender_dust')
+allthemods.add('c:ingots', [
+])
+})
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
