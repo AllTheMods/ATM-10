f2f43fcd65db4b51586df68ffbdd61b885e939cf
20jonas02 <jogajdosik@gmail.com>
2024-08-25 01:25:29 +0200

    Update Crushing_Compressed.js

--- kubejs/server_scripts/mods/Mekanism/Crushing_Compressed.js
+++ kubejs/server_scripts/mods/Mekanism/Crushing_Compressed.js
@@ -5,7 +5,10 @@ ServerEvents.recipes(event => {
     [['cobblestone','gravel'],
      ['gravel','sand']].forEach(recipe =>{
         ['1','2','3','4','5','6','7','8','9'].forEach(count =>{
-            mekCrushing('allthecompressed:'+recipe[0]+'_'+count+'x','allthecompressed:'+recipe[1]+'_'+count+'x','mekanism:crushing_'+recipe[0]+'_to_'+recipe[1]+'_'+count+'x')
+            mekCrushing(
+                'allthecompressed:'+recipe[0]+'_'+count+'x',
+                'allthecompressed:'+recipe[1]+'_'+count+'x',
+                'mekanism:crushing_'+recipe[0]+'_to_'+recipe[1]+'_'+count+'x')
         })
      })
     // jank setup again bc mekanism kubeJS isn't updated
