24639d20a3435ac950b57fa7950524865b210482
Satherov <Satherov@users.noreply.github.com>
2024-08-27 01:37:23 +0200

    MA seeds for ATM seeds

---
+++ kubejs/startup_scripts/CustomAdditions.js
@@ -0,0 +1,18 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+StartupEvents.registry('block', allthemods => {
+    allthemods.create('magical_soil').displayName('§bMagical Soil').grassSoundType().mapColor('grass').hardness(0.6);
+})
+
+StartupEvents.modifyCreativeTab('ironfurnaces:ironfurnaces_tab', allthemods => {
+    allthemods.add('ironfurnaces:allthemodium_furnace')
+    allthemods.add('ironfurnaces:vibranium_furnace')
+    allthemods.add('ironfurnaces:unobtainium_furnace')
+    allthemods.add('ironfurnaces:upgrade_allthemodium')
+    allthemods.add('ironfurnaces:upgrade_vibranium')
+    allthemods.add('ironfurnaces:upgrade_unobtainium')
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
