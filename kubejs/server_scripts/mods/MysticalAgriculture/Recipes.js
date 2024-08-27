5e20af8f9fe128c41d874cb2de5da17140d29569
whatthedrunk <mattmaryanski@gmail.com>
2024-08-26 22:03:05 -0400

    add second recipe for magical soil

--- kubejs/server_scripts/mods/MysticalAgriculture/Recipes.js
+++ kubejs/server_scripts/mods/MysticalAgriculture/Recipes.js
@@ -7,5 +7,20 @@ ServerEvents.recipes(allthemods => {
     })
 })
 
+ServerEvents.recipes(allthemods => {
+    allthemods.shaped('kubejs:magical_soil', ['ABC', 'DEF', 'GHI'], {
+        A: 'mysticalagradditions:insanium_block',
+        B: 'allthecompressed:nether_star_block_2x',
+        C: 'allthecompressed:dirt_3x',
+        D: 'mysticalagriculture:awakened_supremium_growth_accelerator',
+        E: 'mysticalagradditions:insanium_farmland',
+        F: 'minecraft:dragon_head',
+        G: 'allthemodium:piglich_heart',
+        H: 'allthecompressed:ender_pearl_block_3x',
+        I: 'productivetrees:moonlight_magic_crepe_myrtle_sapling'
+    })
+})
+
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
