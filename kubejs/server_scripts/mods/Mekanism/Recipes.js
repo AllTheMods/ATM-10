12a4b07249f40e69d5ca002663f2d56e8dd3ff00
Satherov <Satherov@users.noreply.github.com>
2024-08-30 06:20:02 +0200

    adds a function to generate sawing recipes for all logs of the specified mods

--- kubejs/server_scripts/mods/Mekanism/Recipes.js
+++ kubejs/server_scripts/mods/Mekanism/Recipes.js
@@ -31,7 +31,77 @@ ServerEvents.recipes(allthemods => {
     T: 'mekanismgenerators:bio_generator',
     D: 'mekanism:electrolytic_core'
   }).id('allthemods:mekanismgenerators/gas_burning_gen')
-    
+
+  //mekCrushing({item: 'mod:item', count: 0}, {item/tag: 'mod:item/tag', count: 0}, 'id');
+  function mekCrushing(output, input, id) {
+    let inputObject = {
+      "count": input.count || 1
+    };
+
+    if (input.item) {
+      inputObject["item"] = input.item;
+    } else if (input.tag) {
+      inputObject["tag"] = input.tag;
+    }
+
+    let recipe = {
+      "type": "mekanism:crushing",
+      "input": inputObject,
+      "output": {
+        "count": output.count || 1,
+        "id": output.item
+      }
+    };
+
+    allthemods.custom(recipe).id(`allthemods:mekanism/crushing/${id}`);
+  }
+  //mekSawing({item: 'mod:item', count: 0}, {item/tag: 'mod:item/tag', count: 0}, {chance: 0.00, item: 'mod:item',count: 0}, 'id');
+  function mekSawing(output, input, extraOutput, id) {
+    let inputObject = {
+      "count": input.count || 1
+    };
+
+    if (input.item) {
+      inputObject["item"] = input.item;
+    } else if (input.tag) {
+      inputObject["tag"] = input.tag;
+    }
+
+    let recipe = {
+      "type": "mekanism:sawing",
+      "input": inputObject,
+      "main_output": {
+        "count": output.count || 1,
+        "id": output.item
+      },
+      "secondary_chance": extraOutput.chance,
+      "secondary_output": {
+        "count": extraOutput.count,
+        "id": extraOutput.item
+      }
+    };
+
+    allthemods.custom(recipe).id(`allthemods:mekanism/sawing/${id}`);
+  }
+
+  //list of all the mods to add recipes for
+  const mods = ['croptopia', 'evilcraft', 'eternal_starlight', 'allthemodium',
+    'integrateddynamics', 'ars_nouveau', 'regions_unexplored', 'productivetrees']
+
+  mods.forEach(mod => {
+    //gets all items from the mod with the #minecraft:logs tag
+    let logs = Ingredient.of(`#minecraft:logs`).stacks.toArray().filter(item => item.getId().startsWith(mod + ':')) .map(item => item.getId());
+
+    if(logs.length > 0) {
+      logs.forEach(log => {
+        mekSawing(
+            {item: log.toString().replace('log', 'planks'), count: 6}, //gets the name of the log and replaces the 'log' with 'planks' to generate an output
+            {tag: log + 's', count: 1}, //adds an 's' to the log name to get the tag
+            {chance: 0.25, item: 'mekanism:sawdust', count: 1},
+            mod + '/' + log.toString().split(':').pop() + '_to_planks'); //generates the id by adding the mod name in front of the log and adding a 'to planks' at the end
+      })
+    }
+  })
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
