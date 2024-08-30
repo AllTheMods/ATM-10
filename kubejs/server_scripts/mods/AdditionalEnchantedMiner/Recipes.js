344c5746770b8d43b0de94b7ae281f68420bcd57
djnifos <djnifos@gmail.com>
2024-08-29 20:07:41 -0500

    Add files via upload

---
+++ kubejs/server_scripts/mods/AdditionalEnchantedMiner/Recipes.js
@@ -0,0 +1,17 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+    allthemods.remove({ id: 'quarryplus:quarry' })
+    allthemods.shaped('quarryplus:quarry', ['ABA', 'DED', 'ACA'], {
+      A: 'allthemodium:allthemodium_ingot',
+      B: 'justdirethings:eclipsealloy_pickaxe',
+      C: 'allthemodium:unobtainium_pickaxe',
+      D: 'productivelib:upgrade_productivity_4',
+      E: 'justdirethings:blockbreakert2'
+    })
+  })
+  
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
