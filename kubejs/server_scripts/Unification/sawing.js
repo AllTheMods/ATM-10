bbf364ff313092202495609b139872ab3da83784
Satherov <Satherov@users.noreply.github.com>
2024-09-01 19:47:04 +0200

    moved tags for readability

--- kubejs/server_scripts/Unification/sawing.js
+++ kubejs/server_scripts/Unification/sawing.js
@@ -1,6 +1,35 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
+ServerEvents.tags('item', allthemods => {
+    //regions_unexplored
+    allthemods.add('regions_unexplored:magnolia_logs', [
+        'regions_unexplored:magnolia_log',
+        'regions_unexplored:stripped_magnolia_log',
+        'regions_unexplored:magnolia_wood',
+        'regions_unexplored:stripped_magnolia_wood',
+    ])
+
+    //allthemodium
+    allthemods.add('allthemodium:ancient_logs', [
+        'allthemodium:ancient_log_0',
+        'allthemodium:ancient_log_1',
+        'allthemodium:ancient_log_2',
+        'allthemodium:stripped_ancient_log'
+    ])
+    allthemods.add('allthemodium:soul_logs', [
+        'allthemodium:soul_log',
+        'allthemodium:soul_log_0',
+        'allthemodium:soul_log_1',
+        'allthemodium:soul_log_2',
+        'allthemodium:stripped_soul_log'
+    ])
+    allthemods.add('allthemodium:demonic_logs', [
+        'allthemodium:demonic_log',
+        'allthemodium:stripped_demonic_log',
+    ])
+})
+
 ServerEvents.recipes(allthemods => {
     function mekSawing(output, input, extraOutput, id) {
         let inputObject = {
