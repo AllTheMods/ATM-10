25eade17a55c02bcde4e4e37546c8913eb5a1667
Satherov <Satherov@users.noreply.github.com>
2024-09-06 16:43:50 +0200

    I'm never touching this again

--- kubejs/server_scripts/Unification/sawing.js
+++ kubejs/server_scripts/Unification/sawing.js
@@ -1,13 +1,22 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-ServerEvents.tags('item', allthemods => {
+ServerEvents.tags('tags.item', allthemods => {
     //regions_unexplored
     allthemods.add('regions_unexplored:magnolia_logs', [
         'regions_unexplored:magnolia_log',
         'regions_unexplored:stripped_magnolia_log',
         'regions_unexplored:magnolia_wood',
         'regions_unexplored:stripped_magnolia_wood',
+    ])
+    allthemods.add('regions_unexplored:alpha_logs', [
+        'regions_unexplored:alpha_log'
+    ])
+    allthemods.add('regions_unexplored:silver_birch_logs', [
+        'regions_unexplored:silver_birch_log',
+        'regions_unexplored:stripped_silver_birch_log',
+        'regions_unexplored:silver_birch_wood',
+        'regions_unexplored:stripped_silver_birch_wood',
     ])
 
     //allthemodium
@@ -76,17 +85,24 @@ ServerEvents.recipes(allthemods => {
         }).id(`allthemods:productivetrees/sawing/${id}`);
     }
 
-    let logs  = Ingredient.of('#minecraft:logs').stacks.toArray().map(item => String(item.getId().split(' ').pop()));
+    const excludedMods = ['allthemodium', 'ars_nouveau', 'ars_elemental', 'croptopia', 'eternal_starlight', 'integrateddynamics', 'occultism', 'productivetrees', 'sushigocrafting']
+    const excludedItems = ['regions_unexplored:bamboo_log', 'regions_unexplored:stripped_bamboo_log', 'regions_unexplored:small_oak_log',
+        'regions_unexplored:stripped_small_oak_log', 'regions_unexplored:small_oak_log', 'regions_unexplored:silver_birch_log', 'regions_unexplored:ashen_log', 'regions_unexplored:ashen_wood',]
+    let logs  = Ingredient.of('#minecraft:logs').stacks.toArray().filter(item =>
+        !excludedMods.some(mod => item.getId().startsWith(`${mod}:`)) &&
+        !excludedItems.includes(item.getId())).map(item => String(item.getId()));
     const logTypes = ['log.*', 'stem.*'];
     const logReg = new RegExp(`(_(?:${logTypes.join('|')})(?!.*_(?:${logTypes.join('|')})).*?)$`);
 
     logs.forEach(log => {
+
         mekSawing(
-            {item: log.replace(logReg, '_planks'), count: 6},
-            {tag: log.replace(logReg, '_logs'), count: 1},
+            {item: log.replace(/^(.*?:)(?:stripped_)?(.*?)_(wood|stem|hyphae|log)(?:_.*)?$/, '$1$2_planks'), count: 6},
+            {tag: log.replace(/^(.*?:)(?:stripped_)?(.*?)_(wood|stem|hyphae|log)(?:_.*)?$/, '$1$2_logs'), count: 1},
             {chance: 0.25, item: 'mekanism:sawdust', count: 1},
             log.split(':')[0] + '/' + log.split(':')[1] + '_to_planks'
         );
+
     });
 
     //ars wants to be special
@@ -108,8 +124,8 @@ ServerEvents.recipes(allthemods => {
 
     //ars yet again
     prodSawing(
-        {tag: 'ars_nouveau:archwood_planks'},
-        {item: 'c:logs/archwood', count: 6},
+        {tag: 'c:logs/archwood'},
+        {item: 'ars_nouveau:archwood_planks', count: 6},
         {item: 'productivetrees:sawdust', count: 2},
         'ars_nouveau/archwood_log_to_planks'
     )
