73fd48c05340514ee738483a2727c8fd513a51c0
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2024-09-13 09:52:26 +0300

    remove uneccesary tag fixes remove ptrees saplings from queen bee trades

--- kubejs/server_scripts/mods/The Bumblezone/Tags.js
+++ kubejs/server_scripts/mods/The Bumblezone/Tags.js
@@ -2,12 +2,7 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.tags('item', event => {
-    event.add('functionalstorage:ignore_crafting_check', 'productivebees:draconic_dust')
-    event.add('c:storage_blocks/niter', '#c:storage_blocks/saltpeter')
-    event.add('c:dusts/niter', '#c:dusts/saltpeter')
-})
-ServerEvents.tags('block', event => {
-    event.add('c:storage_blocks/niter', '#c:storage_blocks/saltpeter')
+    event.remove('the_bumblezone:tradeable_saplings', 'productivetrees:alder_sapling')
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
