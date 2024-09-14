28beb618d501124d1a6bcf0f41c06f38915cb933
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2024-09-14 14:04:25 +0300

    fixed sapling removal and added trades for special trees

--- kubejs/server_scripts/mods/The Bumblezone/Tags.js
+++ kubejs/server_scripts/mods/The Bumblezone/Tags.js
@@ -2,7 +2,11 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.tags('item', event => {
-    event.remove('the_bumblezone:tradeable_saplings', 'productivetrees:alder_sapling')
+    const saplings = event.get('minecraft:saplings').getObjectIds()
+    const blacklist = Ingredient.of(/productivetrees.*/)
+    saplings.forEach(sapling => {
+        if (!blacklist.test(sapling)) event.add('the_bumblezone:tradeable_saplings', sapling)
+    })
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
