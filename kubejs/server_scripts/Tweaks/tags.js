ae313c0343ea58b98c409445777a996705439d6a
Satherov <Satherov@users.noreply.github.com>
2024-09-16 06:09:18 +0200

    adds bees to mod duplicator blacklist

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -18,6 +18,7 @@ ServerEvents.tags('item', allthemods => {
 ServerEvents.tags('entity_type', allthemods => {
   // Productive Bees
   allthemods.add('justdirethings:paradox_deny', /productivebees:/);
+  allthemods.add('industrialforegoing:mob_duplicator_blacklist', /productivebees:/)
 });
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
