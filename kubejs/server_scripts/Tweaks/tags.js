b5c6cca67752e54cfb93e03f8293ac2f59332d8f
Satherov <Satherov@users.noreply.github.com>
2024-09-16 14:33:17 +0200

    adds tags to pigliches

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -19,6 +19,11 @@ ServerEvents.tags('entity_type', allthemods => {
   // Productive Bees
   allthemods.add('justdirethings:paradox_deny', /productivebees:/);
   allthemods.add('industrialforegoing:mob_duplicator_blacklist', /productivebees:/)
+
+  //AllTheModium
+  allthemods.add('justdirethings:paradox_deny', 'allthemodium:piglich');
+  allthemods.add('industrialforegoing:mob_duplicator_blacklist', 'allthemodium:piglich')
+
 });
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
