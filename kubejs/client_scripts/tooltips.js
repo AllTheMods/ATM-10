6b74e91db34ec2ae48e217cfb7988ed72a52188b
Satherov <Satherov@users.noreply.github.com>
2024-09-24 21:22:16 +0200

    Forbidden Arcanus Tool-Tips

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -39,9 +39,71 @@ ItemEvents.modifyTooltips(allthemods => {
     ])
 
     //Forbidden Arcanus
+    allthemods.add('forbidden_arcanus:hephaestus_forge_tier_1',[
+        Text.of("§c§lShift-Right-Click§r§c the §c§lSmithing Table§r§c with §lMundabitur Dust"),
+        Text.of("§c█ = Polished Darkstone with Smithing Table on top"),
+        Text.of("§7█ = Polished Darkstone"),
+        Text.of("§5█§7 = Gilded Chiseled Polished Darkstone"),
+        Text.of("§6█§7 = Chiseled Arcane Polished Darkstone"),
+        Text.of("§0███§7███§0███"),
+        Text.of("§0█§7███§5█§7███§0█"),
+        Text.of("§0█§7█§5█§7███§5█§7█§0█"),
+        Text.of("§7████§6█§7████"),
+        Text.of("§7█§5█§7█§6█§c█§6█§7█§5█§7█"),
+        Text.of("§7████§6█§7████"),
+        Text.of("§0█§7█§5█§7███§5█§7█§0█"),
+        Text.of("§0█§7███§5█§7███§0█"),
+        Text.of("§0███§7███§0███")
+
+    ])
+    allthemods.add('forbidden_arcanus:clibano_core',[
+        Text.of("§c§lShift-Right-Click§r§c the §c§lClibano Core§r§c with §c§lMundabitur Dust"),
+        Text.of("§5█§7 = Polished Darkstone"),
+        Text.of("§7█ = Polished Darkstone Bricks"),
+        Text.of("§6█§7 = Clibano Core"),
+        Text.of("§7Right to Left -> Bottom to Top"),
+        Text.of("§5█§7█§5█§0█§7███§0█§5█§7█§5█"),
+        Text.of("§7███§0█§7█§0█§7█§0█§7███"),
+        Text.of("§5█§7█§5█§0█§7█§6█§7█§0█§5█§7█§5█"),
+    ])
     allthemods.add('forbidden_arcanus:growing_edelwood',[
-        Text.of("§4THESE ARE CURRENTLY CRAFTABLE BECAUSE THEY DO NOT SPAWN IN WORLD"),
-        Text.of("§4EXPECT THIS RECIPE TO BE REMOVED ONCE THAT'S FIXED")
+        Text.of("§4Obtainable from the Wandering Trader"),
+        Text.of("§4Or by using a Corrupt Soul on an Oak Sapling"),
+    ])
+    allthemods.add('forbidden_arcanus:magnetized_darkstone_pedestal',[
+        Text.of("§7Use Ferrognetic Mixture on the Darkstone Pedesta"),
+    ])
+    allthemods.add('forbidden_arcanus:soul',[
+        Text.of("§7Use a Soul Extractor on Soul Sand"),
+        Text.of("§7Rarely spawns in world"),
+    ])
+    allthemods.add('forbidden_arcanus:enchanted_soul',[
+        Text.of("§7Use a Splash Aureal Bottle on a normal soul")
+    ])
+    allthemods.add('forbidden_arcanus:corrupt_soul',[
+        Text.of("§7Rarely spawns when killing mobs")
+    ])
+    allthemods.add('forbidden_arcanus:blood_test_tube',[
+        Text.of("§7Hold a test tube in your off-hand and then kill mobs")
+    ])
+    allthemods.add('forbidden_arcanus:xpetrified_orb',[
+        Text.of("§7Only obtainable via the Black Hole"),
+        Text.of("§7To make a Black Hole throw Dark Matter together with Corrupti Dust on the ground"),
+        Text.of("§7Feed it enough xp to make it spit out an Xpetrified Orb")
+    ])
+    allthemods.add('forbidden_arcanus:dragon_scale',[
+        Text.of("§7Dropped by the Ender Dragon")
+    ])
+    allthemods.add('forbidden_arcanus:stella_arcanum',[
+        Text.of("§7Very rarely spawns between Y -44 and Y 42"),
+        Text.of("§cWill explode when you mine it!")
+    ])
+    allthemods.add(/forbidden_arcanus:runic_/,[
+        Text.of("§7Spawns at the bottom of the world up to Y 2"),
+    ])
+    allthemods.add(['forbidden_arcanus:arcane_crystal_ore', 'forbidden_arcanus:deepslate_arcane_crystal_ore'],[
+        Text.of("§7Very rarely spawns between Y -40 and Y 14"),
+        Text.of("§7Most common at Y -13")
     ])
 })
 
