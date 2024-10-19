ca5d79b85dbcfef76921a822860b2b18450ea866
Toblerone0508 <tobybrent10@gmail.com>
2024-10-19 15:11:30 +1100

    Unified Tiny Coals and Charcoals
    Unified the Tiny Coal and Charcoal from AA with the Tiny Coal and Charcoal from Utilitarian.
    Removed duplicate tiny coal recipes.
    Added 2 item tags for Tiny Coal and Tiny Charcoal.

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -2,19 +2,27 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.tags('block', allthemods => {
-  //Waystones
+  // Waystones
   allthemods.add('ftbchunks:interact_whitelist', ['@waystones'])
 
-  //Extreme Reactors
+  // Extreme Reactors
   allthemods.add('c:storage_blocks/yellorium', 'alltheores:uranium_block' )
-  
 })
 
 ServerEvents.tags('item', allthemods => {
-  //Extreme Reactors
+  // Extreme Reactors
   allthemods.add('c:ingots/yellorium', 'alltheores:uranium_ingot' )
   allthemods.add('c:storage_blocks/yellorium', 'alltheores:uranium_block' )
 
+  // Tiny Coal
+  allthemods.add('atm10:tiny_coals', [
+    'utilitarian:tiny_coal',
+    'actuallyadditions:tiny_coal',
+  ])
+  allthemods.add('atm10:tiny_charcoals', [
+    'utilitarian:tiny_charcoal',
+    'actuallyadditions:tiny_charcoal',
+  ])
 })
 
 ServerEvents.tags('entity_type', allthemods => {
@@ -22,10 +30,10 @@ ServerEvents.tags('entity_type', allthemods => {
   allthemods.add('justdirethings:paradox_deny', /productivebees:/);
   allthemods.add('industrialforegoing:mob_duplicator_blacklist', /productivebees:/)
 
-  //AllTheModium
+  // Allthemodium
   allthemods.add('justdirethings:paradox_deny', 'allthemodium:piglich');
   allthemods.add('industrialforegoing:mob_duplicator_blacklist', 'allthemodium:piglich')
-
 });
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
