602747e32a136e236b22efbacdde69b7e919e187
Harvey Hatton-Shaw <harvey@hatton-shaw.com>
2024-10-22 20:06:54 +0100

    actually works

--- kubejs/server_scripts/mods/Pneumaticcraft/recipes.js
+++ kubejs/server_scripts/mods/Pneumaticcraft/recipes.js
@@ -1,12 +1,38 @@
 ServerEvents.recipes(allthemods => {
+
+function changeInput(item, replacement){
 allthemods.replaceInput(
   { input: 'pneumaticcraft:plastic' },
-  'pneumaticcraft:plastic',
-  '#c:plastics'
+  item, replacement
+)}
+
+allthemods.custom(
+  {
+    type: "pneumaticcraft:crafting_shaped_pressurizable",
+    category: "misc",
+    key: {
+      C: {
+        item: "pneumaticcraft:air_canister"
+      },
+      G: {
+        item: "pneumaticcraft:gps_tool"
+      },
+      P: {
+        tag: "c:plastics"
+      }
+    },
+    pattern: [
+      "PPP",
+      "PGP",
+      "PCP"
+    ],
+    result: {
+      count: 1,
+      id: "pneumaticcraft:amadron_tablet"
+    }
+  }
 )
-allthemods.replaceInput(
-  { input: 'pneumaticcraft:plastic_sheets' },
-  'pneumaticcraft:plastic',
-  '#c:plastics'
-)
+
+//changeInput('#pneumaticcraft:plastic_sheets', '#c:plastics')
+changeInput('pneumaticcraft:plastic', '#c:plastics')
 })
