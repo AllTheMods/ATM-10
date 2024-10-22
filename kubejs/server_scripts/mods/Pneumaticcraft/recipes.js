0e9a22cce02bbe39ea1b29897e5c90d8ae648de5
Harvey Hatton-Shaw <harvey@hatton-shaw.com>
2024-10-22 20:19:06 +0100

    remove old recipe

--- kubejs/server_scripts/mods/Pneumaticcraft/recipes.js
+++ kubejs/server_scripts/mods/Pneumaticcraft/recipes.js
@@ -1,38 +1,39 @@
 ServerEvents.recipes(allthemods => {
 
-function changeInput(item, replacement){
-allthemods.replaceInput(
-  { input: 'pneumaticcraft:plastic' },
-  item, replacement
-)}
-
-allthemods.custom(
-  {
-    type: "pneumaticcraft:crafting_shaped_pressurizable",
-    category: "misc",
-    key: {
-      C: {
-        item: "pneumaticcraft:air_canister"
+  function changeInput(item, replacement){
+  allthemods.replaceInput(
+    { input: 'pneumaticcraft:plastic' },
+    item, replacement
+  )}
+  
+  allthemods.remove({mod: 'pneumaticcraft', output: 'pneumaticcraft:amadron_tablet'})
+  allthemods.custom(
+    {
+      type: "pneumaticcraft:crafting_shaped_pressurizable",
+      category: "misc",
+      key: {
+        C: {
+          item: "pneumaticcraft:air_canister"
+        },
+        G: {
+          item: "pneumaticcraft:gps_tool"
+        },
+        P: {
+          tag: "c:plastics"
+        }
       },
-      G: {
-        item: "pneumaticcraft:gps_tool"
-      },
-      P: {
-        tag: "c:plastics"
+      pattern: [
+        "PPP",
+        "PGP",
+        "PCP"
+      ],
+      result: {
+        count: 1,
+        id: "pneumaticcraft:amadron_tablet"
       }
-    },
-    pattern: [
-      "PPP",
-      "PGP",
-      "PCP"
-    ],
-    result: {
-      count: 1,
-      id: "pneumaticcraft:amadron_tablet"
     }
-  }
-)
-
-//changeInput('#pneumaticcraft:plastic_sheets', '#c:plastics')
-changeInput('pneumaticcraft:plastic', '#c:plastics')
-})
+  )
+  
+  //changeInput('#pneumaticcraft:plastic_sheets', '#c:plastics')
+  changeInput('pneumaticcraft:plastic', '#c:plastics')
+  })
