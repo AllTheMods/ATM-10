f619f6fc2e3b784a4888ea0b90b990a41ce680d2
Harvey Hatton-Shaw <harvey@hatton-shaw.com>
2024-10-25 00:33:54 +0100

    gems and formatting

--- kubejs/server_scripts/mods/MysticalAgriculture/Compatibility.js
+++ kubejs/server_scripts/mods/MysticalAgriculture/Compatibility.js
@@ -1,42 +1,50 @@
 ServerEvents.recipes(allthemods => { 
   const ingots = ['aluminum', 'copper', 'iron', 'zinc', 'silver', 'lead', 'brass', 'bronze', 'tin', 'nickel', 'uranium', 'gold', 'osmium', 'invar', 'iridium', 'platinum']
-  const essence = ['prudentium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'imperium', 'imperium', 'imperium', 'imperium', 'imperium', 'supremium', 'supremium']  
+  const ingotsEssence = ['prudentium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'tertium', 'imperium', 'imperium', 'imperium', 'imperium', 'imperium', 'supremium', 'supremium'] 
+  const gems = ['quartz', 'certus_quartz']
+  const gemsEssence = ['tertium', 'tertium']
   
-  for (let i=0; i < ingots.length; i++){  
-
-  //ingots.forEach(ingot => { 
+function mysticalTags(resource, resourceType, essence){
   allthemods.custom({
     type: "mysticalagriculture:infusion",
     input: { item: "mysticalagriculture:prosperity_seed_base" },
     ingredients: [
       {
-        tag: 'c:ingots/'.concat(ingots[i])
+        tag: resourceType.concat(resource)
       },
       {
-        tag: 'c:ingots/'.concat(ingots[i])
+        tag: resourceType.concat(resource)
       },
       {
-        tag: 'c:ingots/'.concat(ingots[i])
+        tag: resourceType.concat(resource)
       },
       {
-        tag: 'c:ingots/'.concat(ingots[i])
+        tag: resourceType.concat(resource)
       },
       {
-        item: 'mysticalagriculture:'.concat(essence[i].concat('_essence'))
+        item: 'mysticalagriculture:'.concat(essence.concat('_essence'))
       },
       {
-        item: 'mysticalagriculture:'.concat(essence[i].concat('_essence'))
+        item: 'mysticalagriculture:'.concat(essence.concat('_essence'))
       },
       {
-        item: 'mysticalagriculture:'.concat(essence[i].concat('_essence'))
+        item: 'mysticalagriculture:'.concat(essence.concat('_essence'))
       },
       {
-        item: 'mysticalagriculture:'.concat(essence[i].concat('_essence'))
+        item: 'mysticalagriculture:'.concat(essence.concat('_essence'))
       }
     ],
     result: {
-      id: 'mysticalagriculture:'.concat(ingots[i].concat('_seeds'))
+      id: 'mysticalagriculture:'.concat(resource.concat('_seeds'))
     }
   })
 }
+
+for (let i=0; i < ingots.length; i++){
+  mysticalTags(ingots[i], 'c:ingots/', ingotsEssence[i])}
+  
+for (let i=0; i < gems.length; i++){
+  mysticalTags(gems, 'c:gems/', gemsEssence)}
+
+
 })
