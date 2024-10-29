9f6ab9c9b0c7367d7e0db18e8f794f9d8412fdb0
Satherov <63814891+Satherov@users.noreply.github.com>
2024-10-29 18:57:04 +0100

    Update Vacuum Freezer.js

--- kubejs/server_scripts/mods/Modern Industrialization/Vacuum Freezer.js
+++ kubejs/server_scripts/mods/Modern Industrialization/Vacuum Freezer.js
@@ -1,4 +1,4 @@
-// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods: Stranded.
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.recipes(allthemods => {
@@ -8,5 +8,5 @@ ServerEvents.recipes(allthemods => {
         .itemOut('minecraft:honey_block')
 })
 
-// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods: Stranded.
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
