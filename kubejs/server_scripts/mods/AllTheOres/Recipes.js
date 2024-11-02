22ce682824bbf92577c9d961bafda601f81c2bad
pr0saic <60778064+pr0saic@users.noreply.github.com>
2024-11-02 06:55:37 +0000

    ore hammer ingots

--- kubejs/server_scripts/mods/AllTheOres/Recipes.js
+++ kubejs/server_scripts/mods/AllTheOres/Recipes.js
@@ -8,7 +8,49 @@ ServerEvents.recipes(allthemods => {
     allthemods.remove({ id: 'allthecompressed:decompress/snow_1x'})
     allthemods.shapeless('9x minecraft:snow_block', ['allthecompressed:snow_1x'])
     allthemods.shapeless('allthecompressed:snow_1x', ['9x minecraft:snow_block'])
+/*
+    //Add ore hammer crushing for other ingots
+    function oreHammer(material){
+        let modID = Ingredient.of()
+        if(!allthemods.countRecipes({ input: 'alltheores:ore_hammers', output: `c:dusts/${material}`})){
+        let recipeID = `${modID}:${material}_dust_from_hammer_${type}`
+        let dust = AlmostUnified.getTagTargetItem(`c:dusts/${material}`)
+        allthemods.shapeless(dust, ['alltheores:ore_hammers'])
+    }}
+*/
+
+const blockedMods = [ 'modern_industrialization', 'mysticalagriculture', 'mysticalagradditions', 'extendedae', 'mekanism' ]
+let ingots = []
+
+Ingredient.of('#c:ingots').itemIds.forEach(id => {
+    let idArr = id.split(":")
+    let material = {resource: idArr[1], mod: idArr[0]}
+    
+    if(!blockedMods.includes(idArr[0])){
+    
+        material.resource = material.resource.replace('ingot_', '')
+        material.resource = material.resource.replace('_ingot', '')
+        
+        if(Item.exists(`${material.mod}:${material.resource}_dust`) || Item.exists(`${material.mod}:dust_${material.resource}`) || Item.exists(`alltheores:${material.resource}_dust`)){ //check if dust for item exists
+            if(!JSON.stringify(ingots).includes(material.resource)){   //check if material already added to array of unique ingots
+                material.dust = `${material.mod}:${material.resource}_dust`
+                ingots.push(material)   //add to array of sorted materials
+            }
+        }
+    }
 })
+//ingots = [resource, mod, dust]
+let dust = ''
+ingots.forEach(id => {
+    if(!allthemods.countRecipes({input: `#c:ingots/${id.resource}`, output: `#c:dusts/${id.resource}`, type: 'minecraft:crafting_shapeless'})){
+        if(AlmostUnified.getTagTargetItem(`c:dusts/${id.resource}`) !== 'minecraft:air'){ dust = AlmostUnified.getTagTargetItem(`c:dusts/${id.resource}`)} else {dust = id.dust}  //prefer unified dust
+        //console.log(id.resource)
+        allthemods.shapeless(dust, [`#c:ingots/${id.resource}`, '#alltheores:ore_hammers']).id(`alltheores:processing/ore_hammer/${id.resource}_dust_from_ingot`)
+    }
+})
+})
+
+
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
