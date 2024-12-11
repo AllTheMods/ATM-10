782efdde31d4152f3dd0449b0c851ff03e599142
SteveHeist <42606649+SteveHeist@users.noreply.github.com>
2024-12-11 05:17:01 -0700

    Create Farmer's Delight/tags.js

---
+++ kubejs/server_scripts/mods/Farmer's Delight/tags.js
@@ -0,0 +1,7 @@
+//This file adds the Pam's Dough tags to the Farmer's Delight Dough
+
+ServerEvents.tags('item', allthemods => {
+  //Dough
+  allthemods.add('c:dough', 'farmersdelight:wheat_dough')
+  allthemods.add('cdough/dough', 'farmersdelight:wheat_dough')
+})
