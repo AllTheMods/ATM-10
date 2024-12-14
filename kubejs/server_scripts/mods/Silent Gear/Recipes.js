f3e90118ca24a13d422f956f75220da68ef55b75
SteveHeist <42606649+SteveHeist@users.noreply.github.com>
2024-12-14 07:40:49 -0700

    Add PneumaticCraft Compressed Iron and Minecraft Chainmail recipes to the Salvager
    Adds recipes to the Salvager for PneumaticCraft Compressed Iron Armor and Minecraft Chainmail armor

--- kubejs/server_scripts/mods/Silent Gear/Recipes.js
+++ kubejs/server_scripts/mods/Silent Gear/Recipes.js
@@ -25,6 +25,64 @@ ServerEvents.recipes(allthemods => {
             }
         }
     )
+    //adds a chainmail salvage, mirroring the crafting recipe from MineColonies, which uses vanilla Iron items rather than Modern Industrialization Iron Rings
+    //Chainmail Helmet
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"minecraft:chainmail_helmet" },
+		    "results": [{"count": 5, "id": "minecraft:iron_nugget" }, {"count":1,"id":"minecraft:iron_ingot"}]
+	    }
+    )
+    //Chainmail Chestplate
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"minecraft:chainmail_chestplate" },
+		    "results": [{"count": 6, "id": "minecraft:iron_nugget" }, {"count":2,"id":"minecraft:iron_ingot"}]
+	    }
+    )
+    //Chainmail Leggings
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"minecraft:chainmail_leggings" },
+		    "results": [{"count": 4, "id": "minecraft:iron_nugget" }, {"count":3,"id":"minecraft:iron_ingot"}]
+	    }
+    )
+    //Chainmail Boots
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"minecraft:chainmail_helmet" },
+		    "results": [{"count": 2, "id": "minecraft:iron_nugget" }, {"count":2,"id":"minecraft:iron_ingot"}]
+	    }
+    )
+    //Adds Pneumaticraft's Compressed Iron Armor to the Salvager
+    //Compressed Iron Helmet
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"pneumaticcraft:compressed_iron_helmet" },
+		    "results": [{"count": 5, "id": "minecraft:leather" }, {"count":5,"id":"pneumaticcraft:ingot_iron_compressed"}]
+	    }
+    )
+    //Compressed Iron Chestplate
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"pneumaticcraft:compressed_iron_chestplate" },
+		    "results": [{"count": 8, "id": "minecraft:leather" }, {"count":8,"id":"pneumaticcraft:ingot_iron_compressed"}]
+	    }
+    )
+    //Compressed Iron Leggings
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"pneumaticcraft:compressed_iron_leggings" },
+		    "results": [{"count": 7, "id": "minecraft:leather" }, {"count":7,"id":"pneumaticcraft:ingot_iron_compressed"}]
+	    }
+    )
+    //Compressed Iron Boots
+    allthemods.custom(
+	    { "type": "silentgear:salvaging",
+		    "ingredient": {"item":"pneumaticcraft:compressed_iron_boots" },
+		    "results": [{"count": 4, "id": "minecraft:leather" }, {"count":4,"id":"pneumaticcraft:ingot_iron_compressed"}]
+	    }
+    )
 	//adds Everything is Copper gear to the Salvager
 	//Copper Pickaxe
     allthemods.custom(
