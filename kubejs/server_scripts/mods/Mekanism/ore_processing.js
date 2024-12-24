19f8fdfbddaef64d263a34850c9417e78f22933a
Satherov <Satherov@users.noreply.github.com.>
2024-12-24 11:38:45 +0100

    adjust ore processing script to ATO changes

--- kubejs/server_scripts/mods/Mekanism/ore_processing.js
+++ kubejs/server_scripts/mods/Mekanism/ore_processing.js
@@ -4,24 +4,6 @@
 
 ServerEvents.recipes(allthemods => {
 
-    const overwrite = [
-        'tin',
-        'lead',
-        'uranium',
-        'osmium',
-    ]
-
-    overwrite.forEach(material => {
-        dissolution(`c:storage_blocks/raw_${material}`, `alltheores:dirty_${material}`, 6000, `mekanism:processing/${material}/slurry/dirty/from_raw_block`)
-        dissolution(`c:raw_materials/${material}`, `alltheores:dirty_${material}`, 2000, `mekanism:processing/${material}/slurry/dirty/from_raw_ore`)
-        dissolution(`c:ores/${material}`, `alltheores:dirty_${material}`, 1000, `mekanism:processing/${material}/slurry/dirty/from_ore`)
-
-        allthemods.remove({id: `mekanism:processing/${material}/crystal/from_slurry`})
-        allthemods.remove({id: `mekanism:processing/${material}/shard/from_crystal`})
-        allthemods.remove({id: `mekanism:processing/${material}/dirty_dust/from_clump`})
-        allthemods.remove({id: `mekanism:processing/${material}/dust/from_dirty_dust`})
-    })
-
     const id = {
         alltheores: [
             'aluminum',
@@ -34,28 +16,20 @@ ServerEvents.recipes(allthemods => {
             'lead',
             'uranium',
             'osmium',
+            'copper',
+            'iron',
+            'gold'
         ],
         allthemodium: [
             'allthemodium',
             'vibranium',
             'unobtainium'
-        ],
-        mekanism: [
-            'iron',
-            'gold',
-            'copper'
         ]
     };
 
     Object.entries(id).forEach(([mod, materials]) => {
         materials.forEach(material => {
 
-            if (mod === 'mekanism') {
-                washing(`mekanism:dirty_${material}`, `mekanism:clean_${material}`, `mekanism:processing/${material}/slurry/clean`);
-                injecting(`c:crystals/${material}`, `mekanism:shard_${material}`, `mekanism:processing/${material}/shard/from_crystal`);
-                crushing(`c:clumps/${material}`, `mekanism:dirty_dust_${material}`, `mekanism:processing/${material}/dirty_dust/from_clump`);
-                enriching(`c:dirty_dusts/${material}`, `alltheores:${material}_dust`, `mekanism:processing/${material}/dust/from_dirty_dust`);
-            }
             if (mod === 'allthemodium') {
                 washing(`allthemodium:dirty_${material}`, `allthemodium:clean_${material}`, `allthemodium:processing/${material}/slurry/clean`);
                 injecting(`c:crystals/${material}`, `allthemodium:${material}_shard`, `allthemodium:processing/${material}/shard/from_crystal`);
@@ -63,34 +37,21 @@ ServerEvents.recipes(allthemods => {
                 enriching(`c:dirty_dusts/${material}`, `allthemodium:${material}_dust`, `allthemodium:processing/${material}/dust/from_dirty_dust`);
             }
             if (mod === 'alltheores') {
+                allthemods.remove({id: `mekanism:processing/${material}/slurry/dirty/from_raw_block`})
+                allthemods.remove({id: `mekanism:processing/${material}/slurry/dirty/from_raw_ore`})
+                allthemods.remove({id: `mekanism:processing/${material}/slurry/dirty/from_ore`})
+                allthemods.remove({id: `mekanism:processing/${material}/crystal/from_slurry`})
+                allthemods.remove({id: `mekanism:processing/${material}/shard/from_crystal`})
+                allthemods.remove({id: `mekanism:processing/${material}/dirty_dust/from_clump`})
+                allthemods.remove({id: `mekanism:processing/${material}/dust/from_dirty_dust`})
+                
                 washing(`alltheores:dirty_${material}`, `alltheores:clean_${material}`, `alltheores:processing/${material}/slurry/from_dirty`);
                 injecting(`c:crystals/${material}`, `alltheores:${material}_shard`, `alltheores:processing/${material}/shard/from_crystal`);
                 crushing(`c:clumps/${material}`, `alltheores:dirty_${material}_dust`, `alltheores:processing/${material}/dirty_dust/from_clumpy`);
-                enriching(`c:dirty_dusts/${material}`, `alltheores:${material}_dust`, `alltheores:enriching/${material}/dust_from_dirty_dust`);
+                enriching(`c:dirty_dusts/${material}`, `alltheores:${material}_dust`, `alltheores:processing/${material}/dust/from_dirty_dust`);
             }
         });
     });
-
-    function dissolution(input, output, amount, id) {
-        allthemods.custom(
-            {
-                "type": "mekanism:dissolution",
-                "chemical_input": {
-                    "amount": 1,
-                    "chemical": "mekanism:sulfuric_acid"
-                },
-                "item_input": {
-                    "count": 1,
-                    "tag": input
-                },
-                "output": {
-                    "amount": amount,
-                    "id": output
-                },
-                "per_tick_usage": true
-            }
-        ).id(id)
-    }
 
     function washing(input, output, id) {
         allthemods.custom(
@@ -117,7 +78,7 @@ ServerEvents.recipes(allthemods => {
             {
                 "type": "mekanism:injecting",
                 "item_input": {
-                    "count": 2,
+                    "count": 1,
                     "tag": input
                 },
                 "chemical_input": {
@@ -125,7 +86,7 @@ ServerEvents.recipes(allthemods => {
                     "chemical": "mekanism:hydrogen_chloride"
                 },
                 "output": {
-                    "count": 1,
+                    "count": 2,
                     "id": output
                 },
                 "per_tick_usage": true
