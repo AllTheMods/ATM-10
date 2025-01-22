062791ee8245eac9b337ed9138f484f23dd08072
Mitchell5200 <mitchell.louth@ozemail.com.au>
2025-01-22 23:33:47 +0800

    Public server chunkloading toggles
    NOT TO BE MERGED TO MAIN
    yet...
    toggles off the loaders that have a config for them, or craft disables the ones that don't

---
+++ kubejs/server_scripts/Pubserverdisable.js
@@ -0,0 +1,6 @@
+/// atm10 public server loaders disable
+ServerEvents.recipes(allthemods => {
+    allthemods.remove({id: 'mekanism:dimensional_stabilizer'})
+    allthemods.remove({id: 'ae2:spatial_anchor'})
+})
+/// atm10 public server loaders disable
