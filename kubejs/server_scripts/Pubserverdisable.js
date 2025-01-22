cb300ac9b4dca4009866fd5e7192502d43282bbd
Mitchell5200 <mitchell.louth@ozemail.com.au>
2025-01-22 23:54:41 +0800

    Update Pubserverdisable.js

--- kubejs/server_scripts/Pubserverdisable.js
+++ kubejs/server_scripts/Pubserverdisable.js
@@ -1,6 +1,8 @@
 /// atm10 public server loaders disable
 ServerEvents.recipes(allthemods => {
     allthemods.remove({id: 'mekanism:dimensional_stabilizer'})
-    allthemods.remove({id: 'ae2:spatial_anchor'})
+    allthemods.remove({id: 'ae2:network/blocks.spatial_anchor'})
+    allthemods.remove({output: 'ae2:spatial_anchor'})  //Brute force, as the above one did not work
 })
 /// atm10 public server loaders disable
+ 
