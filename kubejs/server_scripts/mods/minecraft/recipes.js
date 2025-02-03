ef8e552f075896a7e937a35f8af71fc5b0f50f8f
Toblerone0508 <164738841+TheBedrockMaster@users.noreply.github.com>
2025-02-03 12:55:08 +1100

    Removed Duplicate Cake Recipes

--- kubejs/server_scripts/mods/minecraft/recipes.js
+++ kubejs/server_scripts/mods/minecraft/recipes.js
@@ -2,6 +2,8 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.recipes(allthemods => {
+    allthemods.remove({id: 'minecraft:cake'})
+
     allthemods.shaped(
         Item.of('minecraft:sculk', 1), // arg 1: output
         [
