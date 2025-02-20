bbd1d6716185301efd728eddcdc1b6c697b27dbd
Pietro Lopes <unknownrj@gmail.com>
2025-02-19 21:25:28 -0300

    fix: remove op tiers from mifa

--- kubejs/server_scripts/mods/mifa/recipes.js
+++ kubejs/server_scripts/mods/mifa/recipes.js
@@ -3,20 +3,20 @@
 
 ServerEvents.recipes(allthemods => {
 
-    allthemods.remove('mifa:efficiency_addon_5')
-    allthemods.remove('mifa:efficiency_addon_6')
-    allthemods.remove('mifa:efficiency_addon_7')
-    allthemods.remove('mifa:efficiency_addon_8')
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/efficiency_addon_5'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/efficiency_addon_6'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/efficiency_addon_7'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/efficiency_addon_8'})
 
-    allthemods.remove('mifa:processing_addon_5')
-    allthemods.remove('mifa:processing_addon_6')
-    allthemods.remove('mifa:processing_addon_7')
-    allthemods.remove('mifa:processing_addon_8')
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/processing_addon_5'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/processing_addon_6'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/processing_addon_7'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/processing_addon_8'})
 
-    allthemods.remove('mifa:speed_addon_5')
-    allthemods.remove('mifa:speed_addon_6')
-    allthemods.remove('mifa:speed_addon_7')
-    allthemods.remove('mifa:speed_addon_8')
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/speed_addon_5'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/speed_addon_6'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/speed_addon_7'})
+    allthemods.remove({id: 'industrialforegoing:dissolution_chamber/speed_addon_8'})
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
