62faf43fefc330b560b91fbbc9f972f54d883a1e
Rainer <github@rainerzufall.click>
2025-04-20 19:30:18 +0200

    Update RecipeViewer.js

--- kubejs/client_scripts/RecipeViewer.js
+++ kubejs/client_scripts/RecipeViewer.js
@@ -2,7 +2,6 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 RecipeViewerEvents.removeEntriesCompletely('item', allthemods =>{
-  allthemods.remove('quarryplus:adv_quarry')
   allthemods.remove('allthetweaks:greg_star')
   allthemods.remove('allthetweaks:greg_star_block')
 
