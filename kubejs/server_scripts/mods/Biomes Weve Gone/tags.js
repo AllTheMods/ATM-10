f893fa10fff08589ba191bbdc52996194eeb8157
beckyh2112 <beckyh@republicofnewhome.org>
2025-05-04 18:19:44 -0500

    Create Biomes We've Gone blueberries tags (#2683)
    * Create Biomes We've Gone blueberries tags
    
    Adding useful tags to the blueberries from Biomes We've Gone. It won't make them (yet) usable in blueberry-specific recipes; that will require recipe changes. But it will make them usable in recipes that call for general crops, fruits, or berries.
    
    Should go into its own Biomes We've Gone folder to fit the current organizational structure, but I don't think pull requests let me just add entire folders.
    
    Part the next on my harvestables tag unification quest. AKA the small side-tagging jobs while working on tagging Mama's Herbs and Harvest.
    
    * Update Biomes We've Gone tags
    
    * Rename kubejs/server_scripts/mods/Biomes We've Gone tags to kubejs/server_scripts/mods/Biomes We've Gone/ tags.json
    
    * Rename  tags.json to  tags.js
    
    * Rename  tags.js to tags.js
    
    Third time's the charm.
    
    * Correcting file path

---
+++ kubejs/server_scripts/mods/Biomes Weve Gone/tags.js
@@ -0,0 +1,15 @@
+// Biomes We've Gone; blueberries tag unification
+
+ServerEvents.tags('item', allthemods => {
+
+allthemods.add('c:crops', 'biomeswevegone:blueberries')
+allthemods.add('c:fruits', 'biomeswevegone:blueberries')
+allthemods.add('c:berries', 'biomeswevegone:blueberries')
+allthemods.add('c:berries/blueberry', 'biomeswevegone:blueberries')
+allthemods.add('c:crops/blueberry', 'biomeswevegone:blueberries')
+allthemods.add('c:fruits/blueberries', 'biomeswevegone:blueberries')
+allthemods.add('c:fruits/blueberry', 'biomeswevegone:blueberries')
+allthemods.add('minecolonies:compostables', 'biomeswevegone:blueberries')
+allthemods.add('minecolonies:blacksmith_ingredient_excluded', 'biomeswevegone:blueberries')
+
+})
