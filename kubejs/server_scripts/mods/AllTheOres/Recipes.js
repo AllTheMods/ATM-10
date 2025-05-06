2949780d810c05729a0e9fb8932861786ff42294
Pietro Lopes <unknownrj@gmail.com>
2025-05-06 10:18:05 -0300

    fix (alltheores): ingot to dust recipes

--- kubejs/server_scripts/mods/AllTheOres/Recipes.js
+++ kubejs/server_scripts/mods/AllTheOres/Recipes.js
@@ -3,48 +3,48 @@
 
 ServerEvents.recipes(allthemods => {
 
-    // Snow Fixes
-    allthemods.remove({ id: 'allthecompressed:compress/snow_1x'})
-    allthemods.remove({ id: 'allthecompressed:decompress/snow_1x'})
-    allthemods.shapeless('9x minecraft:snow_block', ['allthecompressed:snow_1x'])
-    allthemods.shapeless('allthecompressed:snow_1x', ['9x minecraft:snow_block']).id(`allthecompressed:compress/snow_1x_manual_only`)
+  // Snow Fixes
+  allthemods.remove({ id: 'allthecompressed:compress/snow_1x' })
+  allthemods.remove({ id: 'allthecompressed:decompress/snow_1x' })
+  allthemods.shapeless('9x minecraft:snow_block', ['allthecompressed:snow_1x'])
+  allthemods.shapeless('allthecompressed:snow_1x', ['9x minecraft:snow_block']).id(`allthecompressed:compress/snow_1x_manual_only`)
 
-const blockedMods = [ 'modern_industrialization', 'mysticalagriculture', 'mysticalagradditions', 'extendedae', 'mekanism' ]
-let ingots = []
+  let $HashSet = Java.loadClass("java.util.HashSet")
 
-Ingredient.of('#c:ingots').itemIds.forEach(id => {
-    let idArr = id.split(":")
-    let material = {resource: idArr[1], mod: idArr[0]}
-    
-    if(!blockedMods.includes(idArr[0])){
-    
-        material.resource = material.resource.replace('ingot_', '')
-        material.resource = material.resource.replace('_ingot', '')
-        
-        if(Item.exists(`${material.mod}:${material.resource}_dust`) || Item.exists(`${material.mod}:dust_${material.resource}`) || Item.exists(`alltheores:${material.resource}_dust`)){ //check if dust for item exists
-            if(!JSON.stringify(ingots).includes(material.resource)){   //check if material already added to array of unique ingots
-                material.dust = `${material.mod}:${material.resource}_dust`
-                if (!Item.exists(material.dust)) {
-		  material.dust = `${material.mod}:dust_${material.resource}`
-		}
-                if (!Item.exists(material.dust)) {
-		  console.warn(`Dust does not exist for material mod: ${material.mod}, resource: ${material.resource}`)
-		} else ingots.push(material)   //add to array of sorted materials
-            }
-        }
+  const blockedMods = new $HashSet(['modern_industrialization', 'mysticalagriculture', 'mysticalagradditions', 'extendedae', 'mekanism'])
+  let materials = Utils.newMap()
+
+  Ingredient.of('#c:ingots').displayStacks.stream().map(stack => stack.idLocation).forEach(id => {
+    let resource = id.path
+    if (!blockedMods.contains(id.namespace)) {
+
+      resource = resource.replace('ingot_', '')
+      resource = resource.replace('_ingot', '')
+
+      let dust = getDustFromMaterial(id.namespace, resource)
+      if (dust == null) return
+      materials.putIfAbsent(resource, dust)
     }
-})
-//ingots = [resource, mod, dust]
-let dust = ''
-ingots.forEach(id => {
-    if(!allthemods.countRecipes({input: `#c:ingots/${id.resource}`, output: `#c:dusts/${id.resource}`, type: 'minecraft:crafting_shapeless'})){
-        if(AlmostUnified.getTagTargetItem(`c:dusts/${id.resource}`) !== 'minecraft:air'){ dust = AlmostUnified.getTagTargetItem(`c:dusts/${id.resource}`)} else {dust = id.dust}  //prefer unified dust
-        //console.log(id.resource)
-        console.log(`Adding dust recipe: ${dust} with resource: ${id.resource}`)
-        allthemods.shapeless(dust, [`#c:ingots/${id.resource}`, '#alltheores:ore_hammers']).id(`alltheores:processing/ore_hammer/${id.resource}_dust_from_ingot`)
+  })
+
+  materials.forEach((resource, dust) => {
+    if (!allthemods.countRecipes({ input: `#c:ingots/${resource}`, output: `#c:dusts/${resource}`, type: 'minecraft:crafting_shapeless' })) {
+      let finalDust = AlmostUnified.getTagTargetItem(`c:dusts/${resource}`)
+      if (finalDust.isEmpty()) finalDust = Item.of(dust)
+      console.log(`Adding dust recipe: ${dust} with resource: ${resource}`)
+      allthemods.shapeless(dust, [`#c:ingots/${resource}`, '#alltheores:ore_hammers']).id(`alltheores:processing/ore_hammer/${resource}_dust_from_ingot`)
     }
+  })
 })
-})
+
+function getDustFromMaterial(mod, resource) {
+  let $Stream = Java.loadClass("java.util.stream.Stream")
+  return $Stream.of([
+    `alltheores:${resource}_dust`,
+    `${mod}:${resource}_dust`,
+    `${mod}:dust_${resource}`
+  ]).filter(str => Item.exists(str)).findFirst().orElse(null)
+}
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
