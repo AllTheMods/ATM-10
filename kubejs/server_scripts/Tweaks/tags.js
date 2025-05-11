e039d976ad0c06635a2321b9d5a712cad91cc49d
Pietro Lopes <unknownrj@gmail.com>
2025-05-11 05:01:37 -0300

    fix: add berserker to better fortress
    Closes #2335

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -132,5 +132,10 @@ ServerEvents.tags('entity_type', allthemods => {
     allthemods.add('justdirethings:paradox_deny', 'mekanism:robit');
 });
 
+ServerEvents.tags('worldgen/structure', allthemods => {
+    // Cataclysm
+    allthemods.add('cataclysm:berserker_spawn', "betterfortresses:fortress");
+});
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
