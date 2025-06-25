c57fedd54ada83717fcff1399345922390237b4a
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-06-25 14:31:00 +0530

    Rebalance: Remove oritech things tier addons 6,7,8 and 9 except processing

---
+++ kubejs/server_scripts/mods/Oritech/recipes.js
@@ -0,0 +1,26 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficient_speed_tier_9'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficient_speed_tier_8'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficient_speed_tier_7'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficient_speed_tier_6'})
+    
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_capacitor_tier_6'})
+
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_acceptor_tier_9'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_acceptor_tier_8'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_acceptor_tier_7'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_acceptor_tier_6'})
+
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficiency_tier_9'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficiency_tier_8'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficiency_tier_7'})
+    allthemods.remove({id: 'oritechthings:atomicforge/addon_block_efficiency_tier_6'})
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
