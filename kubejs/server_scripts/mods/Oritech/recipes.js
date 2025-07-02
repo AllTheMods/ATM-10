cc5891b509f7e1498a3f64e4422fabe05fc35bb8
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-07-02 09:30:08 +0530

    Uncan's fault

--- kubejs/server_scripts/mods/Oritech/recipes.js
+++ kubejs/server_scripts/mods/Oritech/recipes.js
@@ -37,66 +37,6 @@
         allthemods.remove({id: 'oritech:crafting/catalyst'})
         allthemods.remove({id: 'oritech:crafting/enchanter'})
 
-
-        // Oil compatibility
-        allthemods.remove({id: 'oritech:refinery/oilalt'})
-        allthemods.remove({id: 'oritech:refinery/oilbase'})
-
-        allthemods.custom({
-  "type": "oritech:refinery",
-  "fluidInput": {
-    "amount": 1000,
-    "fluid": "#c:crude_oil"
-  },
-  "fluidOutputs": [
-    {
-      "amount": 500,
-      "fluid": "oritech:still_heavy_oil"
-    },
-    {
-      "amount": 250,
-      "fluid": "oritech:still_naphtha"
-    },
-    {
-      "amount": 250,
-      "fluid": "oritech:still_sulfuric_acid"
-    }
-  ],
-  "ingredients": [],
-  "results": [],
-  "time": 120
-})
-    allthemods.custom({
-  "type": "oritech:refinery",
-  "fluidInput": {
-    "amount": 1000,
-    "fluid": "#c:crude_oil"
-  },
-  "fluidOutputs": [
-    {
-      "amount": 500,
-      "fluid": "oritech:still_diesel"
-    },
-    {
-      "amount": 500,
-      "fluid": "oritech:still_naphtha"
-    },
-    {
-      "amount": 500,
-      "fluid": "oritech:still_sulfuric_acid"
-    }
-  ],
-  "ingredients": [
-    {
-      "item": "oritech:clay_catalyst_beads"
-    }
-  ],
-  "results": [],
-  "time": 120
-})
-
-    })
-
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
