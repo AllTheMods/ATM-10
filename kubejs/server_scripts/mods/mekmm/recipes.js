89668e9c4270900589e335bec1c9d2240b1e661c
Satherov <63814891+Satherov@users.noreply.github.com>
2025-07-02 05:45:12 +0200

    feat: mekanism more machine (#3011)
    Co-authored-by: Satherov <Satherov@users.noreply.github.com>

---
+++ kubejs/server_scripts/mods/mekmm/recipes.js
@@ -0,0 +1,27 @@
+﻿// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+
+    allthemods.remove({input: 'mekmm:scrap'})
+    allthemods.remove({output: 'mekmm:scrap'})
+    allthemods.remove({input: 'mekmm:scrap_box'})
+    allthemods.remove({output: 'mekmm:scrap_box'})
+    allthemods.remove({input: 'mekmm:empty_crystal'})
+    allthemods.remove({output: 'mekmm:empty_crystal'})
+    allthemods.remove({input: 'mekmm:uu_matter'})
+    allthemods.remove({output: 'mekmm:uu_matter'})
+    allthemods.remove({input: 'mekmm:ambient_gas_collector'})
+    allthemods.remove({output: 'mekmm:ambient_gas_collector'})
+
+    allthemods.remove({id: 'mekmm:reaction/empty_crystal'})
+    allthemods.remove({id: 'mekmm:oxidizing/uu_matter'})
+    allthemods.remove({id: 'mekmm:compressing/author_doll'})
+
+    allthemods.remove(/mekmm:.*replicat.*/)
+    allthemods.remove(/mekmm:.*recycl.*/)
+    allthemods.remove({type: 'mekmm:recycling'})
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
