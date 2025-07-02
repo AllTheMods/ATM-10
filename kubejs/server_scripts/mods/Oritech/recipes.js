ee6e5614e7b2624364b1854fc2953cd32b1e09bd
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-07-02 09:28:26 +0530

    fix: Standardisation in removing recipes
    Uncan made me do extra work

--- kubejs/server_scripts/mods/Oritech/recipes.js
+++ kubejs/server_scripts/mods/Oritech/recipes.js
@@ -4,7 +4,8 @@
     ServerEvents.recipes(allthemods => {
 
         // Rebalance of 'machine addon extender'
-        allthemods.remove({output: 'oritech:machine_core_3'})
+        allthemods.remove({id: 'oritech:crafting/core3alt'})
+        allthemods.remove({id: 'oritech:crafting/core3'})
         allthemods.shaped(
             Item.of('oritech:machine_core_3', 1),
             [
@@ -17,7 +18,7 @@
                 B: 'oritech:fluxite_block'
             }
         )
-        allthemods.remove({output: 'oritech:machine_extender'})
+        // allthemods.remove({output: 'oritech:machine_extender'})
         allthemods.shaped(
             Item.of('oritech:machine_extender', 1),
             [
@@ -32,8 +33,67 @@
         )
 
         // Remove enchanting stuff
-        allthemods.remove({output: 'oritech:enchantment_catalyst_block'})
-        allthemods.remove({output: 'oritech:enchanter_block'})
+        allthemods.remove({id: 'oritech:crafting/catalyst_alt'})
+        allthemods.remove({id: 'oritech:crafting/catalyst'})
+        allthemods.remove({id: 'oritech:crafting/enchanter'})
+
+
+        // Oil compatibility
+        allthemods.remove({id: 'oritech:refinery/oilalt'})
+        allthemods.remove({id: 'oritech:refinery/oilbase'})
+
+        allthemods.custom({
+  "type": "oritech:refinery",
+  "fluidInput": {
+    "amount": 1000,
+    "fluid": "#c:crude_oil"
+  },
+  "fluidOutputs": [
+    {
+      "amount": 500,
+      "fluid": "oritech:still_heavy_oil"
+    },
+    {
+      "amount": 250,
+      "fluid": "oritech:still_naphtha"
+    },
+    {
+      "amount": 250,
+      "fluid": "oritech:still_sulfuric_acid"
+    }
+  ],
+  "ingredients": [],
+  "results": [],
+  "time": 120
+})
+    allthemods.custom({
+  "type": "oritech:refinery",
+  "fluidInput": {
+    "amount": 1000,
+    "fluid": "#c:crude_oil"
+  },
+  "fluidOutputs": [
+    {
+      "amount": 500,
+      "fluid": "oritech:still_diesel"
+    },
+    {
+      "amount": 500,
+      "fluid": "oritech:still_naphtha"
+    },
+    {
+      "amount": 500,
+      "fluid": "oritech:still_sulfuric_acid"
+    }
+  ],
+  "ingredients": [
+    {
+      "item": "oritech:clay_catalyst_beads"
+    }
+  ],
+  "results": [],
+  "time": 120
+})
 
     })
 
