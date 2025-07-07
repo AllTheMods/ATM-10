029ff7388ba3c4fc9e5c5226a3a38f100686795d
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-07-07 15:51:52 +0530

    feat: added 2 tall vanilla flower recipes
    These recipes use the dye recipes, add nature essence to air slots and the respective dye for the centre item

--- kubejs/server_scripts/mods/MysticalAgriculture/Recipes.js
+++ kubejs/server_scripts/mods/MysticalAgriculture/Recipes.js
@@ -238,6 +238,60 @@ ServerEvents.recipes(allthemods => {
             }
         })
 
+    // 2 Tall flower recipes
+    allthemods.shaped(
+    Item.of('minecraft:rose_bush', 1),
+  [
+    'ABA',
+    'BCA', 
+    'BAA'
+  ],
+  {
+    A: 'mysticalagriculture:nature_essence',
+    B: 'mysticalagriculture:dye_essence',
+    C: 'minecraft:red_dye'
+  }
+)
+    allthemods.shaped(
+    Item.of('minecraft:peony', 1),
+  [
+    'BAA',
+    'ACA', 
+    'BAB'
+  ],
+  {
+    A: 'mysticalagriculture:nature_essence',
+    B: 'mysticalagriculture:dye_essence',
+    C: 'minecraft:pink_dye'
+  }
+)
+    allthemods.shaped(
+    Item.of('minecraft:sunflower', 1),
+  [
+    'AAB',
+    'BCA', 
+    'ABA'
+  ],
+  {
+    A: 'mysticalagriculture:nature_essence',
+    B: 'mysticalagriculture:dye_essence',
+    C: 'minecraft:yellow_dye'
+  }
+)
+    allthemods.shaped(
+    Item.of('minecraft:lilac', 1),
+  [
+    'ABA',
+    'ACB', 
+    'BAA'
+  ],
+  {
+    A: 'mysticalagriculture:nature_essence',
+    B: 'mysticalagriculture:dye_essence',
+    C: 'minecraft:magenta_dye'
+  }
+)
+
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
