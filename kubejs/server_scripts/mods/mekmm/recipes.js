c9228b6419d1b242e685154b0ed9a80c16ec20a3
Satherov <63814891+Satherov@users.noreply.github.com>
2025-07-13 13:24:47 +0200

    feat: remove op machines and recipes from mekmm (#3111)
    Co-authored-by: Satherov <Satherov@users.noreply.github.com>

--- kubejs/server_scripts/mods/mekmm/recipes.js
+++ kubejs/server_scripts/mods/mekmm/recipes.js
@@ -3,24 +3,29 @@
 
 ServerEvents.recipes(allthemods => {
 
-    allthemods.remove({input: 'mekmm:scrap'})
-    allthemods.remove({output: 'mekmm:scrap'})
-    allthemods.remove({input: 'mekmm:scrap_box'})
-    allthemods.remove({output: 'mekmm:scrap_box'})
-    allthemods.remove({input: 'mekmm:empty_crystal'})
-    allthemods.remove({output: 'mekmm:empty_crystal'})
-    allthemods.remove({input: 'mekmm:uu_matter'})
-    allthemods.remove({output: 'mekmm:uu_matter'})
-    allthemods.remove({input: 'mekmm:ambient_gas_collector'})
-    allthemods.remove({output: 'mekmm:ambient_gas_collector'})
+    allthemods.remove({id: 'mekmm:scrap'})
+    allthemods.remove({id: 'mekmm:compressing/scrap_box'})
+    allthemods.remove({id: 'mekmm:empty_crystal'})
+    allthemods.remove({id: 'mekmm:nucleosynthesizing/uu_matter'})
+    allthemods.remove({id: 'mekmm:ambient_gas_collector'})
 
     allthemods.remove({id: 'mekmm:reaction/empty_crystal'})
     allthemods.remove({id: 'mekmm:oxidizing/uu_matter'})
     allthemods.remove({id: 'mekmm:compressing/author_doll'})
 
-    allthemods.remove(/mekmm:.*replicat.*/)
-    allthemods.remove(/mekmm:.*recycl.*/)
+    allthemods.remove({id: /mekmm:.*replicat.*/})
+    allthemods.remove({id: /mekmm:.*recycl.*/})
+    allthemods.remove({id: /mekmm:.*planting.*/})
+    allthemods.remove({id: /mekmm:.*lathing.*/})
+    allthemods.remove({id: /mekmm:.*rolling_mill.*/})
     allthemods.remove({type: 'mekmm:recycling'})
+
+    allthemods.remove({id: /mekmm:compat\/appflux.*/})
+    allthemods.remove({id: /mekmm:compat\/advanced_ae.*/})
+    allthemods.remove({id: /mekmm:compat\/ae2.*/})
+    allthemods.remove({id: /mekmm:compat\/extendedae.*/})
+    allthemods.remove({id: /mekmm:compat\/megacells.*/})
+
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
