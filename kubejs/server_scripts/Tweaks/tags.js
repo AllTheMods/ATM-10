fc1169d8b6d59ddd3fb2406d969f865de889082a
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-07-28 19:56:48 +0530

    using kjs now

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -474,5 +474,10 @@ ServerEvents.tags('block', allthemods => {
   ])
 })
 
+ServerEvents.tags('worldgen/biome', allthemods => {
+    // JustDireThings
+   allthemods.add('justdirethings:unstable_portal_fluid_viable', ["nullscape:crystal_peaks", "nullscape:shadowlands", "nullscape:void_barrens"]);
+});
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
