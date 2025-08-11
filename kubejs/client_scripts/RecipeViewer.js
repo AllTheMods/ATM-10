7be42869361b05120331ae8cbb4e8cdbfc1bdc66
Satherov <satherov@users.noreply.github.com>
2025-08-11 11:27:45 +0200

    yeet²

--- kubejs/client_scripts/RecipeViewer.js
+++ kubejs/client_scripts/RecipeViewer.js
@@ -45,7 +45,7 @@ RecipeViewerEvents.removeEntriesCompletely('item', allthemods => {
     allthemods.remove(/mekmm:.*replicat.*/)
     allthemods.remove(/mekmm:.*recycl.*/)
     allthemods.remove(/mekmm:.*planting.*/)
-    allthemods.remove({id: 'mekmm:cnc_lathe'})
+    allthemods.remove('mekmm:cnc_lathe')
     allthemods.remove(/mekmm:.*lathing.*/)
     allthemods.remove(/mekmm:.*rolling_mill.*/)
 })
