ef1ed6dc32f797ec4359478f3e12f3070b35654b
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2025-09-20 10:08:06 +0530

    added oil compat for crude oil to MI (#3441)

---
+++ kubejs/server_scripts/mods/Modern Industrialization/distillation.js
@@ -0,0 +1,12 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+
+    allthemods.replaceInput({mod: 'modern_industrialization'}
+        , Fluid.ingredientOf("modern_industrialization:crude_oil")
+        , Fluid.ingredientOf("#c:crude_oil"))
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
