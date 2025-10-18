aaa5e6bb38c3ed382f1436af3a223662f4fe6d95
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2025-10-17 21:35:03 -0300

    fix(jei): remove grindstone tab

--- kubejs/client_scripts/RecipeViewer.js
+++ kubejs/client_scripts/RecipeViewer.js
@@ -89,5 +89,9 @@ RecipeViewerEvents.addInformation('fluid', allthemods => {
     ])
 })
 
+RecipeViewerEvents.removeCategories(allthemods => {
+    allthemods.remove(["minecraft:grindstone"])
+})
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
