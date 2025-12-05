ad1440bd530f168303ed9112cd40878fc3d901f2
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2025-12-05 19:24:23 -0300

    fix: fluorite recipe conflict
    Closes #3711

--- kubejs/server_scripts/mods/AllTheOres/Recipes.js
+++ kubejs/server_scripts/mods/AllTheOres/Recipes.js
@@ -9,6 +9,9 @@ ServerEvents.recipes(allthemods => {
   allthemods.shapeless('9x minecraft:snow_block', ['allthecompressed:snow_1x'])
   allthemods.shapeless('allthecompressed:snow_1x', ['9x minecraft:snow_block']).id(`allthecompressed:compress/snow_1x_manual_only`)
 
+  // Fix fluorite recipe conflict
+  allthemods.remove({ id: 'alltheores:processing/fluorite/dust/from_ore' })
+
   let $HashSet = Java.loadClass("java.util.HashSet")
 
   const blockedMods = new $HashSet(['modern_industrialization', 'mysticalagriculture', 'mysticalagradditions', 'extendedae', 'mekanism'])
