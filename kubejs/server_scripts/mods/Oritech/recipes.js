a867d710d5d0c85904f40d1460910df49aa18b58
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2026-01-31 11:14:33 +0530

    bringing back oritech enchanting

--- kubejs/server_scripts/mods/Oritech/recipes.js
+++ kubejs/server_scripts/mods/Oritech/recipes.js
@@ -1,73 +1,67 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-    ServerEvents.recipes(allthemods => {
-        // Oil compatibility
-        allthemods.remove({id: 'oritech:refinery/oilalt'})
-        allthemods.remove({id: 'oritech:refinery/oilbase'})
+ServerEvents.recipes(allthemods => {
+  // Oil compatibility
+  allthemods.remove({ id: 'oritech:refinery/oilalt' })
+  allthemods.remove({ id: 'oritech:refinery/oilbase' })
 
-        allthemods.custom({
-            "type": "oritech:refinery",
-            "fluidInput": {
-                "amount": 1000,
-                "fluid": "#c:crude_oil"
-            },
-            "fluidOutputs": [
-                {
-                    "amount": 500,
-                    "fluid": "oritech:still_heavy_oil"
-                },
-                {
-                    "amount": 250,
-                    "fluid": "oritech:still_naphtha"
-                },
-                {
-                    "amount": 250,
-                    "fluid": "oritech:still_sulfuric_acid"
-                }
-            ],
-            "ingredients": [],
-            "results": [],
-            "time": 120
-        })
-        allthemods.custom({
-            "type": "oritech:refinery",
-            "fluidInput": {
-                "amount": 1000,
-                "fluid": "#c:crude_oil"
-            },
-            "fluidOutputs": [
-                {
-                    "amount": 500,
-                    "fluid": "oritech:still_diesel"
-                },
-                {
-                    "amount": 500,
-                    "fluid": "oritech:still_naphtha"
-                },
-                {
-                    "amount": 500,
-                    "fluid": "oritech:still_sulfuric_acid"
-                }
-            ],
-            "ingredients": [
-                {
-                    "item": "oritech:clay_catalyst_beads"
-                }
-            ],
-            "results": [],
-            "time": 120
-        })
-        // Remove enchanting stuff
-        // allthemods.remove({id: 'oritech:crafting/catalyst_alt'})
-        // allthemods.remove({id: 'oritech:crafting/catalyst'})
-        // allthemods.remove({id: 'oritech:crafting/enchanter'})
+  allthemods.custom({
+    "type": "oritech:refinery",
+    "fluidInput": {
+      "amount": 1000,
+      "fluid": "#c:crude_oil"
+    },
+    "fluidOutputs": [
+      {
+        "amount": 500,
+        "fluid": "oritech:still_heavy_oil"
+      },
+      {
+        "amount": 250,
+        "fluid": "oritech:still_naphtha"
+      },
+      {
+        "amount": 250,
+        "fluid": "oritech:still_sulfuric_acid"
+      }
+    ],
+    "ingredients": [],
+    "results": [],
+    "time": 120
+  })
+  allthemods.custom({
+    "type": "oritech:refinery",
+    "fluidInput": {
+      "amount": 1000,
+      "fluid": "#c:crude_oil"
+    },
+    "fluidOutputs": [
+      {
+        "amount": 500,
+        "fluid": "oritech:still_diesel"
+      },
+      {
+        "amount": 500,
+        "fluid": "oritech:still_naphtha"
+      },
+      {
+        "amount": 500,
+        "fluid": "oritech:still_sulfuric_acid"
+      }
+    ],
+    "ingredients": [
+      {
+        "item": "oritech:clay_catalyst_beads"
+      }
+    ],
+    "results": [],
+    "time": 120
+  })
 
-        allthemods.replaceInput({id: 'oritech:crafting/catalyst'}, )
-
-        // Cheaty alloys
-        allthemods.remove({id: 'oritech:crafting/alloy/steel'})
-        allthemods.remove({id: 'oritech:crafting/alloy/electrum'})
+  // Cheaty alloys
+  allthemods.remove({ id: 'oritech:crafting/alloy/steel' })
+  allthemods.remove({ id: 'oritech:crafting/alloy/electrum' })
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
