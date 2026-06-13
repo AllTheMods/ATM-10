33db4e5b5d3edd8cefd81443c913275f18e0a36a
Abbage230 <ryoi16207@gmail.com>
2026-06-14 00:46:58 +0900

    Change custom text to translation key

--- kubejs/client_scripts/Mekanism-Tooltips.js
+++ kubejs/client_scripts/Mekanism-Tooltips.js
@@ -7,87 +7,87 @@ ItemEvents.modifyTooltips(allthemods => {
 
     //Mekasuit
     allthemods.add(/mekanism:mekasuit_/, [
-        Text.red('Increased Energy Consumption!'),
-        Text.green('Increased Energy Capacity')
+        Text.red(Text.translate('tooltip.atm.measuit_r')),
+        Text.green(Text.translate('tooltip.atm.measuit_g'))
     ])
     //Meka Tool
     allthemods.add('mekanism:meka_tool', [
-        Text.red('Increased Energy Consumption!'),
-        Text.green('Increased Energy Capacity!'),
-        Text.green('Increased Attack Speed & Damage!')
+        Text.red(Text.translate('tooltip.atm.meka_tool_r')),
+        Text.green(Text.translate('tooltip.atm.meka_tool_g1')),
+        Text.green(Text.translate('tooltip.atm.meka_tool_g2'))
     ])
 
     // ##### Generators #####
 
     //Solar Generator
     allthemods.add('mekanismgenerators:solar_generator', [
-        Text.green('Increased Energy Capacity & Production!')
+        Text.green(Text.translate('tooltip.atm.solar_generator_g'))
     ])
     //Advanced Solar Generator
     allthemods.add('mekanismgenerators:advanced_solar_generator', [
-        Text.green('Increased Energy Capacity & Production!')
+        Text.green(Text.translate('tooltip.atm.advanced_solar_generator_g'))
     ])
     //Wind Generator
     allthemods.add('mekanismgenerators:wind_generator', [
-        Text.green('Increased Energy Capacity & Production!')
+        Text.green(Text.translate('tooltip.atm.wind_generator_g'))
     ])
     //Heat Generator
     allthemods.add('mekanismgenerators:heat_generator', [
-        Text.green('Increased Energy Capacity & Production!')
+        Text.green(Text.translate('tooltip.atm.heat_generator_g'))
     ])
     //Gas Burning Generator
     allthemods.add('mekanismgenerators:gas_burning_generator', [
-        Text.red('Decreased Energy Production!'),
-        Text.red('Increased Fuel Consumption!')
+        Text.red(Text.translate('tooltip.atm.gas_burning_generator_r1')),
+        Text.red(Text.translate('tooltip.atm.gas_burning_generator_r2'))
     ])
     //Fission Generator
     allthemods.add(/mekanismgenerators:fission_/, [
-        Text.red('Decreased Energy Production!'),
+        Text.red(Text.translate('tooltip.atm.fission_r')),
     ])
     //Fusion Generator
     allthemods.add(/mekanismgenerators:fusion_/, [
-        Text.red('Decreased Energy Production!'),
-        Text.green('Decreased Fuel Consumption!'),
+        Text.red(Text.translate('tooltip.atm.fusion_r')),
+        Text.green(Text.translate('tooltip.atm.fusion_g')),
     ])
     //Turbine
     allthemods.add(/mekanismgenerators:turbine_/, [
-        Text.green('Increased Production Speed!'),
+        Text.green(Text.translate('tooltip.atm.turbine_g')),
     ])
     //Boiler
     allthemods.add(/mekanism:boiler_/, [
-        Text.green('Increased Production Speed!'),
+        Text.green(Text.translate('tooltip.atm.boiler_g')),
     ])
 
     // ##### Machines #####
 
     //Upgrades
     allthemods.add(/mekanism:upgrade_/, [
-        Text.green('Increased Machine Boost!')
+        Text.green(Text.translate('tooltip.atm.upgrade_g'))
     ])
     //Waste Barrel
     allthemods.add('mekanism:radioactive_waste_barrel', [
-        Text.green('Increased Decay Rate!')
+        Text.green(Text.translate('tooltip.atm.radioactive_waste_barrel_g'))
     ])
     //Thermal Evaporation Tower
     allthemods.add(/mekanism:thermal_evaporation_/, [
-        Text.green('Increased Production Speed!')
+        Text.green(Text.translate('tooltip.atm.thermal_evaporation_g'))
     ])
     //Solar Neutron Activator
     allthemods.add('mekanism:solar_neutron_activator', [
-        Text.green('Increased Production Speed!'),
-        Text.green('Waste -> Polonium buffed!')
+        Text.green(Text.translate('tooltip.atm.solar_neutron_activator_g1')),
+        Text.green(Text.translate('tooltip.atm.solar_neutron_activator_g2'))
     ])
     //Isotopic Centrifuge
     allthemods.add('mekanism:isotopic_centrifuge', [
-        Text.green('Waste -> Plutonium buffed!')
+        Text.green(Text.translate('tooltip.atm.isotopic_centrifuge_g'))
     ])
     //Electric Pump
     allthemods.add('mekanism:electric_pump', [
-        Text.green('Increased Production Speed!')
+        Text.green(Text.translate('tooltip.atm.electric_pump_g'))
     ])
     //SPS
     allthemods.add(/mekanism:sps_/, [
-        Text.green('Decreased Energy Consumption!')
+        Text.green(Text.translate('tooltip.atm.sps_g'))
     ])
 })
 
